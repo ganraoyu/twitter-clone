afc4e3177fa63ed0285b43d3986aea5eabe12a12
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-08-15 13:35:04 -0400

    updated

---
+++ backend/database/connectMongoDB.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectMongoDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = connectMongoDB; // If you want to use this function elsewhere
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/auth.routes.js'); 
 const dotenv = require('dotenv');
+const { connect } = require('mongoose');
+const connectMongoDB = require('./database/connectMongoDB.js');
 
-// Load environment variables from a custom path
+
 dotenv.config({ path: '../.env' });
 
-// Now access the environment variable
-console.log(process.env.MONGO_URI); // This should correctly log the value of TEST from the .env file
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use('/api/auth', authRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
 });

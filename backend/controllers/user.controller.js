235a44b72a4880c38f614fb267f10c66e16f5835
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-08-20 19:50:15 -0400

    updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user.model');
+
 const getUserProfile = async (req, res) => {
     const { username } = req.params.username;
     try { 
@@ -11,4 +13,30 @@ const getUserProfile = async (req, res) => {
     }
 };
 
-module.exports = { getUserProfile };
+const followUnfollowUser = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const userToModify = await User.findById(id);
+        const currentUser = await User.findById(req.user._id);
+
+        if(id === req.user._id) {
+            return res.status(400).json({error: 'You cannot follow yourself'});
+        }
+
+        if (!userToModify || !currentUser) {
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        const isFollow = currerntUser.followig.includes(id);
+
+        if (isFollowing){
+
+        } else{
+            
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Server error'});
+    }
+};    
+module.exports = { getUserProfile, followUnfollowUser };

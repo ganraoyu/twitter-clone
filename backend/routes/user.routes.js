6d4d33b841bbe465edadc082ad05a162562bf0a0
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-08-20 19:28:22 -0400

    updated

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { protectRoute } = require('../middleware/protectRoute');
 
 const router = express.Router();
+
+router.get('/profile/:username', protectRoute, getUserProfile);
+router.get('/suggested', protectRoute, getUserProfile);
+router.post('/follow/:userId', protectRoute, followUnfollowUser);
+router.post('/update', protectRoute, updateUserProfile);
 
 module.exports = router;

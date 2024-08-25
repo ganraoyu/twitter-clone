afd16cc910f42fa87d56cb3bd7a07e1000c4e7ad
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-08-24 22:28:13 -0400

    updated

---
+++ backend/models/post.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    text: {
+        type: String,
+    },
+
+    img: {
+        type: String,
+    },
+
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    comments: [
+        {
+            text: {String, required: true},
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        },
+    ]
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', postSchema);

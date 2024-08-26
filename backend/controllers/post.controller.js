13c417062755a59ab5f5a95fe65a509a08615929
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-08-25 21:11:56 -0400

    updated

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,7 +2,7 @@ const Post = require('../models/post.model.js');
 const cloudinary = require('cloudinary').v2;
 const User = require('../models/user.model.js');
  
- const createPost = async (req, res) => {
+const createPost = async (req, res) => {
     try{
         const { text } = req.body;
         let { img } = req.body;
@@ -36,4 +36,26 @@ const User = require('../models/user.model.js');
     }
 }
 
-module.exports = createPost;
+const deletePost = async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({ message: 'Post not found' });
+        }
+        if(post.user.toString() !== req.user._id.toString()){
+            return res.status(401).json({ message: 'You are not authorized to delete this post' });
+        }
+
+        if(post.img){
+            const imgId = post.img.split('/').pop().split('.')[0];
+            await cloudinary.uploader.destroy(imgId);
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+module.exports = { createPost, deletePost };

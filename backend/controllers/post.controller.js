fc59e58ebff62bd950a31cb8b7631be367818e79
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-08-28 22:34:54 -0400

    updated

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -171,6 +171,16 @@ const getFollowingPosts = async (req, res) => {
     try{
         const userId = req.user._id;
         const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const following = user.following;
+
+        const feedPosts = await Post.find({ user: { $in: following } })
+        .sort({ createdAt: -1 })
+        .populate('user', '-password')
+        .populate('comments.user', '-password');
     } catch (error){
         console.log('Error getting following posts:', error);
         res.status(500).json({ message: 'Server error' });

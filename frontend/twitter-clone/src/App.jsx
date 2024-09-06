a26aa9b7b9a92dd60d4007d2af359f9a24270932
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-09-06 13:14:51 -0400

    updated

--- frontend/twitter-clone/src/App.jsx
+++ frontend/twitter-clone/src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="flex max-w-6x1 mx-auto">
+      <Routes>
+        
+      </Routes>
+    </div>
   )
 }
 

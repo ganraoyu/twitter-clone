0aa2d2ea230671f46ffbcf9fc708d9908e4b4740
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-09-06 17:39:14 -0400

    updated

--- frontend/twitter-clone/src/App.jsx
+++ frontend/twitter-clone/src/App.jsx
@@ -1,10 +1,16 @@
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import LoginPage from './pages/auth/login/LoginPage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+
 function App() {
 
   return (
     <div className="flex max-w-6xl mx-auto">
       <Routes>
-        
+        <Route path="/" element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='signup' element={<SignUpPage />} />
       </Routes>
     </div>
   )

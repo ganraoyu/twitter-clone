ab092d207266c4d0f9e41c5a31f33ba7d5af1f46
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-09-06 09:47:25 -0400

    updated

--- frontend/twitter-clone/src/App.jsx
+++ frontend/twitter-clone/src/App.jsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
+    <h1 className="text-3xl text-blue-500 bg-blue-300 font-bold underline">
       Hello world!
     </h1>
   )

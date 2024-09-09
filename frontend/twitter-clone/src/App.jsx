1604ebc848e24d51d44953a843266774e427c4ad
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-09-08 21:31:57 -0400

    updated

--- frontend/twitter-clone/src/App.jsx
+++ frontend/twitter-clone/src/App.jsx
@@ -2,16 +2,20 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import LoginPage from './pages/auth/login/LoginPage'
 import SignUpPage from './pages/auth/signup/SignUpPage'
+import Sidebar from './components/common/SideBar'
+import RightPanel from './components/common/RightPanel.jsx'
 
 function App() {
 
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='signup' element={<SignUpPage />} />
       </Routes>
+      <RightPanel />
     </div>
   )
 }
